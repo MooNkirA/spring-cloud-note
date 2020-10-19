dda1d6ee0fde8f073c4c012ebbae7b30068fb77f
MooNkirA <lje888@126.com>
2020-10-19 16:04:12 +0800

    feat: hystrix 实时监控与 DashBoard 监控平台、聚合监控 Turbine 的使用示例；

--- spring-cloud-greenwich-sample/09-springcloud-hystrix-feign/shop-service-order/src/main/java/com/moon/order/OrderApplication.java
+++ spring-cloud-greenwich-sample/09-springcloud-hystrix-feign/shop-service-order/src/main/java/com/moon/order/OrderApplication.java
@@ -3,6 +3,8 @@ package com.moon.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -16,6 +18,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication(scanBasePackages = "com.moon.order")
 @EntityScan("com.moon.entity") // 指定扫描实体类的包路径
 @EnableFeignClients // 开启Feign的支持
+@EnableCircuitBreaker // 开启Hystrix支持
+@EnableHystrixDashboard // 激活监控平台（仪表盘）
 public class OrderApplication {
 
     public static void main(String[] args) {
