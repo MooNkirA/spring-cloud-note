43002a5ca5e9431e8c032f4a2cc4690c503ec788
MooNkirA <lje888@126.com>
2020-11-22 09:37:52 +0800

    feat: Spring Cloud Stream 基础入门示例

---
+++ spring-cloud-greenwich-sample/15-springcloud-stream/stream-producer/src/test/java/com/moon/stream/test/ProducerTest.java
@@ -0,0 +1,31 @@
+package com.moon.stream.test;
+
+import com.moon.stream.message.MessageSender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 消息生产者测试类
+ *
+ * @author MooNkirA
+ * @version 1.0
+ * @date 2020-11-15 13:41
+ * @description
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProducerTest {
+
+    @Autowired
+    private MessageSender messageSender;
+
+    /* 测试发送消息 */
+    @Test
+    public void sendMessage() {
+        messageSender.send("Hello, Spring Cloud Stream Producer!");
+    }
+
+}
