14865a390b16277d8a74cc085aa94e4ba59fd037
MooNkirA <lje888@126.com>
2020-11-28 16:04:12 +0800

    feat: Spring Cloud Config 基础入门示例与手动刷新配置示例

---
+++ spring-cloud-greenwich-sample/16-springcloud-config/server-config/src/main/java/com/moon/config/ConfigServerApplication.java
@@ -0,0 +1,23 @@
+package com.moon.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Config Server 启动类
+ *
+ * @author MooNkirA
+ * @version 1.0
+ * @date 2020-11-28 10:58
+ * @description
+ */
+@SpringBootApplication
+@EnableConfigServer // 开启注册中心服务端功能
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
